cf0514814ce97363ef8a92f16f01eba351916ef8
Ming Duan <11203duan@gmail.com>
2019-03-16 21:54:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,23 @@
 class Song
 
-
+  
 
   def initialize(name, artist, genre)
     @name = name
+    @artist = artist
+    @genre = genre
   end
 
   def name
     @name
   end
 
+  def artist
+    @artist
+  end
+
+  def genre
+    @genre
+  end
 
 end
